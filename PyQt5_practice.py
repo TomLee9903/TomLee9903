28cb11f203dbe9fcb60d79c8cfbdcad83a90482b
TomLee9903 <whdgur1454@gmail.com>
2022-02-24 23:47:21 +0900

    [Src] PyQt5 practice1 업로드

--- PyQt5_practice.py
+++ PyQt5_practice.py
@@ -1,9 +1,16 @@
 import sys
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5 import uic
+
+form_class = uic.loadUiType("test.ui")[0]
+
+class MyWindow(QMainWindow, form_class):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
 
 app = QApplication(sys.argv)
-
-window = QWidget()
+window = MyWindow()
 window.show()
-
 app.exec_()
