49baeaf45a2002aa1643749b24a384647e9b84ce
Tom Lee <whdgur7408@naver.com>
2022-02-25 08:38:56 +0900

    [Src] Qt designer로 편집한 ui 파일을 Python code에서 불러오는 것까지 성공

--- PyQt5_practice.py
+++ PyQt5_practice.py
@@ -3,14 +3,27 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5 import uic
 
-form_class = uic.loadUiType("test.ui")[0]
+form_class = uic.loadUiType("main_window.ui")[0]
 
-class MyWindow(QMainWindow, form_class):
+class MyWindow(QDialog, form_class):
     def __init__(self):
         super().__init__()
+        self.setUI()
+        
+    def setUI(self):
         self.setupUi(self)
 
-app = QApplication(sys.argv)
-window = MyWindow()
-window.show()
-app.exec_()
+        
+# class MyWindow(QMainWindow, form_class): // Qt Designer form template에 따라 상속 class도 달라짐 체크
+#     def __init__(self):
+#         super().__init__()
+#         self.setUI()
+        
+#     def setUI(self):
+#         self.setupUi(self)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MyWindow()
+    window.show()
+    app.exec_()
