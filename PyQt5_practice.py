bc87bc44434c90e447de86a004f26ed1c1268173
Tom Lee <whdgur7408@naver.com>
2022-02-24 22:54:17 +0900

    Add PyQt5 practice

---
+++ PyQt5_practice.py
@@ -0,0 +1,9 @@
+import sys
+from PyQt5.QtWidgets import *
+
+app = QApplication(sys.argv)
+
+window = QWidget()
+window.show()
+
+app.exec_()
--- test.py
+++ test.py
@@ -1,1 +1,23 @@
-import pandas
+from selenium import webdriver as wd
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import re
+import json
+import pandas as pd
+
+user_id = "id"
+user_password = "password"
+login_option = "facebook"
+driver_path = "~/chromedriver"
+instagram_id_name = "username"
+instargram_pw_name = "password"
+instagram_login_btn = ".sqdOP.L3NKy.y3zKF   "
+facebook_login_page_css = ".sqdOP.L3NKy.y3zKF   "
+facebook_ligin_page_css2 = ".sqdOP.yWX7d.y3zKF   "
+facebook_id_from_name = "email"
+facebook_pw_from_name = "pass"
+facebook_login_btn_name = "login"
+
+print()
